90eb8050e42127d9fa86904690345d11443451bc
JoJo <ketema.galy@gmail.com>
2018-07-23 12:43:30 +0300

    bug fixes

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ import torchvision.transforms as transforms
 import torchvision.models as models
 import torchvision.datasets as datasets
 
-from models import get_unsupervised_model, load_model, Retrieval_Model
+from models import get_unsupervised_model, load_model, Retrieval_Model, SalRet_Model
 from datasets import Training_Contrastive_Dataset
 from losses import ContrastiveLoss
 from training import train
 from validation import calculate_map
 from downloading import datasets_download
+from cleaning import clean
 
 from PIL import ImageFile
 
@@ -29,16 +30,16 @@ parser = argparse.ArgumentParser('SAL-MAC training and testing')
 parser.add_argument('--data', metavar='DIR', default='../landmarks-clean/data_train')
 parser.add_argument('--labels', default = '../landmarks-clean/labels.csv')
 parser.add_argument('--arch', '-a', metavar='ARCH',default='alexnet')
-parser.add_argument('--pretrained', '-pt', metavar='PT', default=True)
-parser.add_argument('--train', '-t', metavar='TRAIN', default=False)
+parser.add_argument('--pretrained', '-pt', metavar='PT', default=False)
+parser.add_argument('--train', '-t', metavar='TRAIN', default=True)
 parser.add_argument('--pool', default = 'mac')
 parser.add_argument('--loss', default='contrastive')
-parser.add_argument('--epochs', default=30)
+parser.add_argument('--epochs', default=120)
 parser.add_argument('--lr', '-lr', default=0.0001)
 parser.add_argument('--batch_size', '-bs', default=32)
 parser.add_argument('--optimizer', '-o', default = 'adam')
 parser.add_argument('--saliency', '-s', default = '', type = str)
-parser.add_argument('--sal-supervised', '-ss', default = False)
+parser.add_argument('--salsupervised', '-ss', default = '')
 parser.add_argument('--val_dataset', default = 'oxford5k')
 
 transform = transforms.Compose([
@@ -52,10 +53,11 @@ def main():
     pretrained = args.pretrained
     pool = args.pool
     model = Retrieval_Model(load_model(arch, pretrained), pool)
-
+    if args.salsupervised is not '':
+        model = SalRet_Model(model).cuda()
     sal_model = None
-    if args.saliency is not '':
-        sal_model = get_unsupervised_model('rbd')
+    #if args.saliency is not '' and args.:
+    #    sal_model = get_unsupervised_model(args.saliency)
     if args.train:
         datadir = args.data
         loss = args.loss
--- models.py
+++ models.py
@@ -34,6 +34,50 @@ class Joint_Model(nn.Module):
         x = self.ret_model(x)
         return x
 
+class SalRet_Model(nn.Module):
+    def __init__(self, model):
+        super(SalRet_Model, self).__init__()
+        self.model = model
+
+        self.conv1 = nn.Conv2d(3, 16, kernel_size = 7, stride = 3)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size = 5, stride = 2)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size = 3, stride = 1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.deconv1 = nn.ConvTranspose2d(64, 32, kernel_size = 3, stride = 1)
+        self.bn4 = nn.BatchNorm2d(32)
+        self.deconv2 = nn.ConvTranspose2d(32, 16, kernel_size = 5, stride = 2)
+        self.bn5 = nn.BatchNorm2d(16)
+        self.deconv3 = nn.ConvTranspose2d(16, 1, kernel_size = 7, stride = 3)
+
+        self.pool = nn.MaxPool2d(2, 2, return_indices=True)
+        self.unpool = nn.MaxUnpool2d(2, 2)
+
+    def forward(self, x):
+        image = x
+        x = F.elu(self.bn1(self.conv1(x)))
+        x, inds1 = self.pool(x)
+        s1 = x.size()
+
+        x = F.elu(self.bn2(self.conv2(x)))
+        x, inds2 = self.pool(x)
+        s2 = x.size()
+
+        x = F.elu(self.bn3(self.conv3(x)))
+        x = F.elu(self.bn4(self.deconv1(x)))
+        x = F.upsample_bilinear(x, size = (s2[2], s2[3]))
+
+        x = F.elu(self.bn5(self.deconv2(x)))
+        x = F.upsample_bilinear(x, size = (s1[2], s1[3]))
+
+        x = F.sigmoid(self.deconv3(x))
+        ims = image.size()
+        x = F.upsample_bilinear(x, size = (ims[2], ims[3]))
+        sal_app = x * image
+        return self.model(sal_app)
+        
+        
 def load_model(arch, pretrained):
     arch = arch.lower()
     if 'vgg' in arch:
@@ -54,5 +98,7 @@ def get_unsupervised_model(model_type):
     if 'rbd' in model_type:
         return get_saliency_rbd
     elif 'ft' in model_type:
-        return get_saliency_ft        
+        return get_saliency_ft   
+    elif 'mbd' in model_type:
+        print('model {} is not yet supported'.format(model_type))
 
